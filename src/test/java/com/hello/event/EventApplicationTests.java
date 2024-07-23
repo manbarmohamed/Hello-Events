ad489b7d04589061f97228e09f3ff163b09bca3b
manbarmohamed <you@example.com>
2024-07-23 10:14:22 +0100

    project file

---
+++ src/test/java/com/hello/event/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
