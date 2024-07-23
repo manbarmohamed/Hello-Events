24122e57db9e6f29c7dd7beda2530881c04f4d9d
manbarmohamed <you@example.com>
2024-07-24 00:38:56 +0100

    realise les methode suivantes ==> (save reservation, send contact request, find contact by id et get all contact dans controller )

---
+++ src/main/java/com/hello/event/controller/ContactController.java
@@ -0,0 +1,35 @@
+package com.hello.event.controller;
+
+
+import com.hello.event.model.Contact;
+import com.hello.event.service.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/contact")
+public class ContactController {
+
+    private final ContactService contactService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Contact>> getContacts() {
+        return ResponseEntity.ok(contactService.getAllContacts());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Contact> saveContact(Contact contact) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(contactService.save(contact));
+    }
+
+    @GetMapping("/{contact_id}")
+    public Contact findContactById( @PathVariable String contact_id) {
+        return contactService.getContactById(Long.parseLong(contact_id));
+    }
+}
