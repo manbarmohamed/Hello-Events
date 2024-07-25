fc5562ef255c00b3bec714d0c603457c2ac0908a
iima23 <elmazouz.imane@gmail.com>
2024-07-25 17:22:16 +0100

    add tests

--- src/main/java/com/hello/event/controller/ReservationController.java
+++ src/main/java/com/hello/event/controller/ReservationController.java
@@ -6,6 +6,7 @@ import com.hello.event.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
     private final ReservationService reservationService;
 
+    @PreAuthorize("hasRole('CLIENT')")
     @PostMapping("/add")
     public ResponseEntity<Reservation> save(Reservation reservation) {
         return ResponseEntity.ok(reservationService.save(reservation));
